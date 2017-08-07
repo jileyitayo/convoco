bae64a52f4f39c64202d08ebf6ac0282d389162b
Jesuloluwa Eyitayo <jileyitayo@gmail.com>
2017-08-08 00:02:23 +0100

    Editted the Laravel Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Convoco
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laracasts.com">Our Videos</a>
+                    <a href="https://laravel-news.com">Short News</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">My GitHub</a>
                 </div>
             </div>
         </div>
