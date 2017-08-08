7704157e6119a0331dc7a29775d1bd8603011da9
Jesuloluwa Eyitayo <jileyitayo@gmail.com>
2017-08-08 22:41:05 +0100

    edited the users migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(191);
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('profilepicture')->nullable()->default('default.jpg');
+            $table->string('status')->nullable();
+            $table->string('phonenumber')->nullable();
+            $table->timestamp('date_of_birth');
             $table->rememberToken();
             $table->timestamps();
         });
