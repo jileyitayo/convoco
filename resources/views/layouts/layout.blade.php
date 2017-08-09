95226b96c1af7d7d2b927cb47efb62e4aca7d5e1
Jesuloluwa Eyitayo <jileyitayo@gmail.com>
2017-08-09 22:05:32 +0100

    added layout template file - 'layout'

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'convoco') }}</title>
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="{{ asset('css/flexslider.css') }}">
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- Custom style  -->
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+    <!-- Modernizr JS -->
+    <script src="{{ asset('js/modernizr-2.6.2.min.js') }} "></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="{{ asset('js/respond.min.js') }}"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="fh5co-page">
+    <!-- <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a> -->
+    <aside id="fh5co-aside" role="complementary" class="border js-fullheight">
+
+        <h1 id="fh5co-logo"><a href="{{ url('/') }}">{{ config('app.name', 'convoco') }}</a></h1>
+        <nav id="fh5co-main-menu" role="navigation">
+            <ul>
+                @if (Auth::guest())
+                    <li class="fh5co-active"><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+                {{--<li class="fh5co-active"><a href="index.html">Home</a></li>--}}
+                {{--<li><a href="blog.html">Blog</a></li>--}}
+                {{--<li><a href="portfolio.html">Portfolio</a></li>--}}
+                {{--<li><a href="about.html">About</a></li>--}}
+                {{--<li><a href="contact.html">Contact</a></li>--}}
+            </ul>
+        </nav>
+
+        <div class="fh5co-footer">
+            <p><small>&copy; <script type="text/javascript">document.write(new Date().getFullYear());</script> {{ config('app.name', 'convoco') }}. All Rights Reserved.</small></p>
+            <ul>
+                <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                <li><a href="#"><i class="icon-twitter2"></i></a></li>
+                <li><a href="#"><i class="icon-instagram"></i></a></li>
+                <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+            </ul>
+        </div>
+    </aside>
+
+    <div id="fh5co-page">
+        @yield('content')
+    </div>
+</div>
+    <!-- jQuery -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <!-- jQuery Easing -->
+    <script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <!-- Waypoints -->
+    <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+    <!-- Flexslider -->
+    <script src="{{ asset('js/jquery.flexslider-min.js') }}"></script>
+
+
+    <!-- MAIN JS -->
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
