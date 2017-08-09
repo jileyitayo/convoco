9af33346988c442f52289822547a9d28c5c8781f
Jesuloluwa Eyitayo <jileyitayo@gmail.com>
2017-08-09 12:15:28 +0100

    referenced groups that belongs to user

--- app/Group.php
+++ app/Group.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'groupname', 'groupstatus', 'created_by',
+    ];
+
+    public function createdby(){
+        return $this->belongsTo('App\User');
+    }
 }
