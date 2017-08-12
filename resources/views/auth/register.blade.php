d26b7eeda4ce85c09d81ec35dc9f08aa6087f2dc
Jesuloluwa Eyitayo <jileyitayo@gmail.com>
2017-08-12 13:20:02 +0100

    corrected the color issue of the password validation #registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,7 +36,7 @@
 										</span>
 									@endif
 								</div>
-								<div class="form-group-md{ $errors->has('password') ? ' has-error' : '' }}">
+								<div class="form-group-md{{ $errors->has('password') ? ' has-error' : '' }}">
 									<label>Password</label>
 									<input id="password" type="password" class="form-control" name="password" required>
 									@if ($errors->has('password'))
