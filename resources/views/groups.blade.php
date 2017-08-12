f6f420d80ca7bfeb31ecbb9cc1fd446007d69c4f
Jesuloluwa Eyitayo <jileyitayo@gmail.com>
2017-08-12 03:11:49 +0100

    more ui  and asset files added

---
+++ resources/views/groups.blade.php
@@ -0,0 +1,174 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Convoco</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Custom style  -->
+	<link rel="stylesheet" href="css/custom.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	<div id="fh5co-page">
+		<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+		<aside id="fh5co-aside" role="complementary" class="border js-fullheight">
+
+			<h1 id="fh5co-logo2"><a href="index.php">Convoco</a></h1>
+			<div class="text-center">
+				<img src="images/me.jpg" class="profile-pic img-circle">				
+				<p><b>{{Name Here}}</b></p>
+				<br><br>
+			</div>
+			<nav id="fh5co-main-menu" role="navigation">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li class="fh5co-active"><a href="groups.html">Groups</a></li>
+					<li><a href="profile.html">Profile</a></li>
+				</ul>
+			</nav>
+
+			<div class="fh5co-footer">
+				<p><small>&copy; <script type="text/javascript">document.write(new Date().getFullYear());</script> Convoco. All Rights Reserved.</span></small></p>
+				<ul>
+					<li><a href="#"><i class="icon-facebook2"></i></a></li>
+					<li><a href="#"><i class="icon-twitter2"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+					<li><a href="#"><i class="icon-linkedin2"></i></a></li>
+				</ul>
+			</div>
+
+		</aside>
+
+		<div id="fh5co-main">
+			<div class="fh5co-narrow-content">				
+				<div class="row">
+					<div class="col-md-offset-4 col-md-4 fh5co-add-group text-center">
+						<div class="add">
+							<img src="images/add.jpg" class="img-circle" data-toggle="modal" data-target="#addGroup">
+						</div>
+						<p><br><b>Click "Add Group" to invite family and friends.</b></p>
+						<p>>> Test <a href="#" data-toggle="modal" data-target="#inviteModal">"Invite"</a> link</p>
+					</div>
+				</div>				
+			</div>	
+		</div>
+	</div>
+
+	<!-- Modal for adding the Group name and description -->
+	<div id="addGroup" class="modal fade" role="dialog">
+  		<div class="modal-dialog modal-md">
+
+    		<!-- Modal content-->
+    		<div class="modal-content">
+      			<div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal">&times;</button>
+			        <h4 class="modal-title">Create New Group</h4>
+      			</div>
+      			<div class="modal-body">
+        			<form>
+        				<div class="form-group">
+        					<label>Group Name</label>
+        					<input type="text" name="groupname" placeholder="Choose a Group Name" class="form-control">
+        				</div>
+        				<div class="form-group">
+        					<label>Group Description</label>
+        					<textarea name="groupdesc" placeholder="Enter a description" class="form-control"></textarea>
+        				</div>
+        				<div class="form-group text-center">
+        					<button type="submit" name="create" class="btn btn-lg btn-success"><b>Create Group</b></button>
+        				</div>
+        			</form>
+      			</div>
+      			<div class="modal-footer">
+      			</div>
+    		</div>
+  		</div>
+	</div>
+
+	<!-- Modal for inviting new members -->
+	<div id="inviteModal" class="modal fade" role="dialog">
+  		<div class="modal-dialog modal-md">
+
+    		<!-- Modal content-->
+    		<div class="modal-content">
+      			<div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal">&times;</button>
+			        <h4 class="modal-title">New Invite</h4>
+      			</div>
+      			<div class="modal-body">
+        			<form>
+        				<div class="form-group">
+        					<label>Email</label>
+        					<input type="text" name="email" placeholder="Enter invitee's email" class="form-control">
+        				</div>
+        				<div class="form-group text-center">
+        					<button type="submit" name="create" class="btn btn-lg btn-success"><b>Invite</b></button>
+        				</div>
+        			</form>
+      			</div>
+      			<div class="modal-footer">
+      			</div>
+    		</div>
+  		</div>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	
+	
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
