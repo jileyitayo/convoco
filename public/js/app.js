faf193361b99637a7c2d340aec9b22a7b84891f1
Jesuloluwa Eyitayo <jileyitayo@gmail.com>
2017-08-14 16:34:26 +0100

    added app.js for ajax functions

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by JIL on 14/08/2017.
+ */
+;(function () {
+
+    // group functions
+    var alertme = function() {
+
+    };
+
+
+    // Document on load.
+    $(function(){
+        alertme();
+    });
+}());
